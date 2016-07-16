163a864a097886e2b5ad1f88cd95599e7a94df5f
Dong <jellylidong@users.noreply.github.com>
2016-07-16 00:13:46 -0700

    [101. Symmetric Tree][Accepted]committed by LeetCode Extension

--- 101-Symmetric-Tree/solution.js
+++ 101-Symmetric-Tree/solution.js
@@ -12,14 +12,36 @@
 var isSymmetric = function(root) {
     if(root === null)
         return true;
-    var helper = function(p, q){
+        
+    //recursion    
+    // var helper = function(p, q){
+    //     if(p === null && q === null)
+    //         return true;
+    //     if((p === null && q !== null) || (p !== null && q === null))
+    //         return false;
+    //     if(p.val != q.val)
+    //         return false;
+    //     return helper(p.left, q.right) && helper(p.right, q.left);
+    // }
+    // return helper(root.left, root.right);
+    
+    //iteraion
+    var queue = [];
+    queue.push(root.left);
+    queue.push(root.right);
+    while(queue.length > 0){
+        p = queue.shift();
+        q = queue.shift();
         if(p === null && q === null)
-            return true;
+            continue;
         if((p === null && q !== null) || (p !== null && q === null))
             return false;
         if(p.val != q.val)
             return false;
-        return helper(p.left, q.right) && helper(p.right, q.left);
+        queue.push(p.left);
+        queue.push(q.right);
+        queue.push(p.right);
+        queue.push(q.left);
     }
-    return helper(root.left, root.right);
+    return true;
 };
