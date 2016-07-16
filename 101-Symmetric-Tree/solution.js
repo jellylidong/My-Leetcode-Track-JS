dff75cb7f70f2060691988416867fdf4424c2c5f
Dong <jellylidong@users.noreply.github.com>
2016-07-16 00:03:13 -0700

    [101. Symmetric Tree][Accepted]committed by LeetCode Extension

---
+++ 101-Symmetric-Tree/solution.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if(root === null)
+        return true;
+    var helper = function(p, q){
+        if(p === null && q === null)
+            return true;
+        if((p === null && q !== null) || (p !== null && q === null))
+            return false;
+        if(p.val != q.val)
+            return false;
+        return helper(p.left, q.right) && helper(p.right, q.left);
+    }
+    return helper(root.left, root.right);
+};
