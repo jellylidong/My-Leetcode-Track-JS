811d6ec2153c5c19ded1e69bc1795ec6127ed622
Dong <jellylidong@users.noreply.github.com>
2016-07-17 12:57:22 -0700

    [114. Flatten Binary Tree to Linked List][Accepted]committed by LeetCode Extension

---
+++ 114-Flatten-Binary-Tree-to-Linked-List/solution.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function(root) {
+    
+    var pre = null;
+    var helper = function(root){
+        if(root === null)    return null;
+        var left = root.left;
+        var right = root.right;
+        root.left = null;
+        if(left !== null)   root.right = helper(left);
+        else pre = root;
+        pre.right = helper(right)
+        
+        return root;
+    }
+    helper(root);
+};
