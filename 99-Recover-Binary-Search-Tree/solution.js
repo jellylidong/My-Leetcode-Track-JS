89287d2110bf5ad275c65adc57bdf068b9cfff20
Dong <jellylidong@users.noreply.github.com>
2016-07-17 10:46:20 -0700

    [99. Recover Binary Search Tree][Accepted]committed by LeetCode Extension

--- 99-Recover-Binary-Search-Tree/solution.js
+++ 99-Recover-Binary-Search-Tree/solution.js
@@ -10,15 +10,15 @@
  * @return {void} Do not return anything, modify root in-place instead.
  */
 var recoverTree = function(root) {
-    //variant of inorder traversal
     var n1 = null;
     var n2 = null;
-    var cur = root;
     var pre = null;
-    var traverse = function(root){
+    
+    var helper = function(root){
         if(root === null)
             return;
-        traverse(root.left);
+        helper(root.left);
+        
         //for the two swapped node, one must be smaller than it's root, and the other is bigger than its root
         //since we are doing inorder traversal, the first one we can find must be the bigger-than-pre one
         //so let n1 = pre, 
@@ -27,15 +27,14 @@ var recoverTree = function(root) {
         if(n1 === null && pre !== null && pre.val >= root.val)
             n1 = pre;
         if(n1 !== null && pre.val >= root.val)
-            n2 = root; //!!!!!!!!! root, not pre
+            n2 = root;
             
         pre = root;
-        traverse(root.right);
+        helper(root.right);
     }
-    traverse(cur);
-    // console.log(n1.val + " " + n2.val);
-    tmp = n1.val;
+    
+    helper(root);
+    var tmp = n1.val;
     n1.val = n2.val;
     n2.val = tmp;
-    // console.log(n1.val + " " + n2.val);
 };
