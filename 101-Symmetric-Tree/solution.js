8c7df51dfef53afdc5960cc88bcbf526e3b25f7a
Dong <jellylidong@users.noreply.github.com>
2016-07-19 00:13:56 -0700

    [101. Symmetric Tree][Accepted]committed by LeetCode Extension

--- 101-Symmetric-Tree/solution.js
+++ 101-Symmetric-Tree/solution.js
@@ -10,38 +10,13 @@
  * @return {boolean}
  */
 var isSymmetric = function(root) {
-    if(root === null)
-        return true;
-        
-    //recursion    
-    // var helper = function(p, q){
-    //     if(p === null && q === null)
-    //         return true;
-    //     if((p === null && q !== null) || (p !== null && q === null))
-    //         return false;
-    //     if(p.val != q.val)
-    //         return false;
-    //     return helper(p.left, q.right) && helper(p.right, q.left);
-    // }
-    // return helper(root.left, root.right);
-    
-    //iteraion
-    var queue = [];
-    queue.push(root.left);
-    queue.push(root.right);
-    while(queue.length > 0){
-        p = queue.shift();
-        q = queue.shift();
-        if(p === null && q === null)
-            continue;
-        if((p === null && q !== null) || (p !== null && q === null))
-            return false;
-        if(p.val != q.val)
-            return false;
-        queue.push(p.left);
-        queue.push(q.right);
-        queue.push(p.right);
-        queue.push(q.left);
+    if(root === null)   return true;
+    var helper = function(r1, r2){
+        if(r1 === null && r2 === null)  return true;
+        if(r1 === null && r2 !== null)  return false;
+        if(r1 !== null && r2 === null)  return false;
+        if(r1.val != r2.val)    return false;
+        return helper(r1.left, r2.right) && helper(r1.right, r2.left); 
     }
-    return true;
+    return helper(root.left, root.right);
 };
