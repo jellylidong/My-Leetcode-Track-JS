81f14ff976491c2cdcccb4a1b6ce9e9125771ace
Dong <jellylidong@users.noreply.github.com>
2016-07-19 17:18:15 -0700

    [207. Course Schedule][Accepted]committed by LeetCode Extension

--- 207-Course-Schedule/solution.js
+++ 207-Course-Schedule/solution.js
@@ -4,39 +4,40 @@
  * @return {boolean}
  */
 var canFinish = function(n, pre) {
-    //seem LC can not use require, it can not use hashmap
-    var HashMap = require("hashmap");
-    var preCount = new HashMap(); //count of pre-req
-    var followList = new HashMap();
-    for(var i = 0; i < n; i++){
+    var preCount = new Map();
+    var followList = new Map();
+    for(let i = 0; i < n; i++){
         preCount.set(i, 0);
         followList.set(i, []);
     }
-    for( i = 0; i < pre.length; i++){
-        var course = pre[i][0];
-        var req = pre[i][1];
+    
+    for(let i = 0; i < pre.length; i++){
+        let course = pre[i][0];
+        let preReq = pre[i][1];
         preCount.set(course, preCount.get(course)+1);
-        followList.get(req).push(course);
+        followList.get(preReq).push(course);
     }
-    console.log(preCount);
-    console.log(followList);
+    // console.log(followList);
     var q = [];
+    // console.log(preCount);
     preCount.forEach(function(value, key){
         if(value === 0) q.push(key);
-    });
+    })
+    
     while(q.length > 0){
-        var size = q.length;
-        for(i = 0; i < size; i++){
-            course = q.shift();
-            n--;
-            var list = followList.get(course);
-            for(i = 0; i < list.length; i++){
-                preCount.set(list[i], preCount.get(list[i])-1);
-                if(preCount.get(list[i]) === 0)
-                    q.push(list[i]);
+        let size = q.length;
+        n -= size;
+        for(let i = 0; i < size; i++){
+            let list = followList.get(q.shift());
+            for(let j = 0; j < list.length; j++){
+                preCount.set(list[j], preCount.get(list[j])-1);
+                if(preCount.get(list[j]) === 0)
+                    q.push(list[j]);
             }
         }
+        // console.log(preCount);
     }
-    
+    console.log(n)
     return n === 0;
+    
 };
