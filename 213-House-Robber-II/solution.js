624765dae7aa393d57ef040d8546d4f9721f1e96
Dong <jellylidong@users.noreply.github.com>
2016-07-25 16:48:45 -0700

    [213. House Robber II][Accepted]committed by LeetCode Extension

--- 213-House-Robber-II/solution.js
+++ 213-House-Robber-II/solution.js
@@ -4,21 +4,31 @@
  */
 var rob = function(nums) {
     var helper = function(nums, lo, hi){
-        var dp = [];
-        for(let i = 0; i <= hi-lo; i++){
-            dp[i] = [];
-            dp[i][0] = 0; //not rob i
-            dp[i][1] = 0; //rob i
-        }
+        // var dp = [];
+        // for(let i = 0; i <= hi-lo; i++){
+        //     dp[i] = [];
+        //     dp[i][0] = 0; //not rob i
+        //     dp[i][1] = 0; //rob i
+        // }
 
-        dp[0][0] = 0;
-        dp[0][1] = nums[lo];
-        for(let i = 1; i <= hi-lo; i++){
-            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]);
-            dp[i][1] = nums[i+lo] + dp[i-1][0];
+        // dp[0][0] = 0;
+        // dp[0][1] = nums[lo];
+        // for(let i = 1; i <= hi-lo; i++){
+        //     dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]);
+        //     dp[i][1] = nums[i+lo] + dp[i-1][0];
+        // }
+        // console.log(dp)
+        // return Math.max(dp[hi-lo][0], dp[hi-lo][1]);
+        
+        var robPre = nums[lo];
+        var nonPre = 0;
+        for(let i = lo+1; i <= hi; i++){
+            var robCur = nums[i] + nonPre;
+            var nonCur = Math.max(robPre, nonPre);
+            nonPre = nonCur;
+            robPre = robCur;
         }
-        console.log(dp)
-        return Math.max(dp[hi-lo][0], dp[hi-lo][1]);
+        return Math.max(robPre, nonPre);
     }
     var len = nums.length;
     if(len === 0)   return 0;
