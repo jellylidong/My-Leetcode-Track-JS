28e6ba104471ad79d13365626568b656217e3d48
Dong <jellylidong@users.noreply.github.com>
2016-07-25 14:41:10 -0700

    [132. Palindrome Partitioning II][Accepted]committed by LeetCode Extension

---
+++ 132-Palindrome-Partitioning-II/solution.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minCut = function(s) {
+    var count = []; //count[i] means the min cut of s.substring(0, i+1)
+    for(let i = 0; i < s.length; i++)
+        count[i] = i;
+    
+    var len = s.length;
+    for(let i = 0; i < len; i++){
+        //assume the pal is odd length, then use s[i] as the mid char of the pal
+        //update the count while s[i-x..i..i+x] is pal
+        let offset = 0;
+        while(i-offset >= 0 && i+offset < len && s[i-offset] == s[i+offset]){
+            count[i+offset] = Math.min(count[i+offset], i-offset === 0? 0:count[i-offset-1]+1);
+            offset++;
+        }
+        
+        //assume the pal is even length, then use s[i] as the end of left half of the pal
+        //update the count while s[i-x..i,i+1..i+1+x] is pal
+        offset = 0;
+        while(i-offset >= 0 && i+1+offset < len && s[i-offset] == s[i+1+offset]){
+            count[i+1+offset] = Math.min(count[i+1+offset], i-offset === 0? 0:count[i-offset-1]+1);
+            offset++;
+        }
+    }
+    
+    return count[len-1];
+};
