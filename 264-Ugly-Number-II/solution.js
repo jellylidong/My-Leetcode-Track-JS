5b1421b9b9152f49ce260001303df0ead17b7708
Dong <jellylidong@users.noreply.github.com>
2016-07-25 17:19:50 -0700

    [264. Ugly Number II][Accepted]committed by LeetCode Extension

--- 264-Ugly-Number-II/solution.js
+++ 264-Ugly-Number-II/solution.js
@@ -6,10 +6,11 @@ var nthUglyNumber = function(n) {
     var dp = [1, 1];
     var i2 = 1, i3 = 1, i5 = 1;
     for(let i = 2; i <= n; i++){
-        dp[i] = Math.min(dp[i2]*2, Math.min(dp[i3]*3, dp[i5]*5));
-        if(dp[i2]*2 <= dp[i])   i2++;
-        if(dp[i3]*3 <= dp[i])   i3++;
-        if(dp[i5]*5 <= dp[i])   i5++;
+        var n2 = dp[i2]*2, n3 = dp[i3]*3, n5 = dp[i5]*5;
+        dp[i] = Math.min(n2, Math.min(n3, n5));
+        if(n2 <= dp[i])   i2++;
+        if(n3 <= dp[i])   i3++;
+        if(n5 <= dp[i])   i5++;
     }
     // console.log(dp);
     return dp[n];
