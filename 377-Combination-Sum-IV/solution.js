2afdde59d50224786ecdfa5259834254ce3a839d
Dong <jellylidong@users.noreply.github.com>
2016-07-30 12:02:32 -0700

    [377. Combination Sum IV][Accepted]committed by LeetCode Extension

--- 377-Combination-Sum-IV/solution.js
+++ 377-Combination-Sum-IV/solution.js
@@ -4,20 +4,32 @@
  * @return {number}
  */
 var combinationSum4 = function(nums, target) {
-    var map = new Map();
+    //method 1: top-down DP
+    // var map = new Map();
+    // var helper = function(nums, target){
+    //     if(target < 0) return 0;
+    //     if(target === 0)    return 1;
+    //     if(map.has(target))
+    //         return map.get(target);
+    //     var ans = 0;
+    //     for(let i = 0; i < nums.length; i++){
+    //         ans += helper(nums, target-nums[i]);
+    //     }
+    //     map.set(target, ans);
+    //     return ans;
+    // }
+    // return helper(nums, target);
     
-    var helper = function(nums, target){
-        if(target < 0) return 0;
-        if(target === 0)    return 1;
-        if(map.has(target))
-            return map.get(target);
-        var ans = 0;
-        for(let i = 0; i < nums.length; i++){
-            ans += helper(nums, target-nums[i]);
+    //method 2: bottom-up DP
+    var dp = [];
+    for(let i = 0; i <= target; i++)
+        dp.push(0);
+    dp[0] = 1;
+    for(let i = 1; i <= target; i++){
+        for(let j = 0;  j < nums.length; j++){
+            if(i - nums[j] >= 0)
+                dp[i] += dp[i - nums[j]];
         }
-        map.set(target, ans);
-        return ans;
     }
-    
-    return helper(nums, target);
+    return dp[target];
 };
