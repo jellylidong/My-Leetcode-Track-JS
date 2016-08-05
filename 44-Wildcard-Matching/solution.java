211972f4f525844ceab5c57aa4ff1a9bd1096bcf
Dong <jellylidong@users.noreply.github.com>
2016-08-04 23:12:49 -0700

    [44. Wildcard Matching][Accepted]committed by LeetCode Extension

--- 44-Wildcard-Matching/solution.java
+++ 44-Wildcard-Matching/solution.java
@@ -1,26 +1,44 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-        var sId = 0, pId = 0;
-        var preSId = 0, prePId = -1;
         
-        while(sId < s.length){
-            if(pId < p.length && (p[pId] == '?' || p[pId] == s[sId])){
-                pId++;
-                sId++;
-            }
-            else if(pId < p.length && p[pId] = '*'){
-                prePId = pId;
-                preSId = sId;
-                pId++;
-            }
-            else if(prePId != -1){
-                pId = prePId + 1;
-                preSId++;
-                sId = preSId;
-            }
-            else    return false;
+        int lo = 0;
+        while(lo < p.length() && p.charAt(lo) == '*'){
+            lo++;
         }
-        while(pId < p.length)
+        if(lo > 0)  lo--;
+        
+        int hi = p.length()-1;
+        while(lo < hi && p.charAt(hi) == '*'){
+            hi--;
+        }
+        if(hi < p.length()-1) hi++;
+        
+        p = p.substring(lo, hi+1);
+        
+        int ls = s.length();
+        int lp = p.length();
+        boolean[][] dp = new boolean[ls+1][lp+1]; // s.substring(i, ls) matches p.substring(j, lp)
+        dp[ls][lp] = true; //empty matches empty
+        
+        //!!!!!!!!
+        if(lp-1 >= 0)
+            dp[ls][lp-1] = p.charAt(lp-1) =='*';//for case s = "aa", p = "*"
+            
+             
+        for(int i = ls-1; i >= 0; i--){
+            for(int j = lp-1; j >= 0; j--){
+                if(s.charAt(i) == p.charAt(j))
+                    dp[i][j] = dp[i+1][j+1];
+                else if(p.charAt(j) == '?')
+                     dp[i][j] = dp[i+1][j+1];
+                else if(p.charAt(j) == '*')
+                    dp[i][j] = dp[i+1][j] || dp[i][j+1];
+                    // * already matches a string, s.charAt(i) just adds a char to the string|| * works as empty
+            }
+        }
+        
+        return dp[0][0];
+        
         
     }
 }
