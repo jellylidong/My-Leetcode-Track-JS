dbcc414168f639677b5bca3691293dd46548a2c8
Dong <jellylidong@users.noreply.github.com>
2016-08-13 16:50:37 -0700

    [31. Next Permutation][Accepted]committed by LeetCode Extension

--- 31-Next-Permutation/solution.java
+++ 31-Next-Permutation/solution.java
@@ -3,6 +3,7 @@ public class Solution {
         if(nums.length <= 1)    return;
         int i = nums.length-1;
         //step 1: find the first peak from right
+        //then nums[i...len-1] is in descending order
         while(i-1 >= 0){
             if(nums[i-1] < nums[i]) break;
             else i--;
@@ -15,18 +16,26 @@ public class Solution {
             return;
         }
         
+        //step 2
+        //find the right most number in nums[i...len-1] that is > nums[i-1]
+        //and swap it with nums[i-1]
+        //so that the next permutation is minimum
         int minId = nums.length-1;
         while(minId >= i){
+            //since nums[i...len-1] is in descending order
+            //so while we are going from right to left, the first number that nums[minId] > nums[i-1]
+            //is the position we are looking for to swap with nums[i-1]
             if(nums[minId] > nums[i-1])
                 break;
             minId--;
         }
-        
-        
         int tmp = nums[i-1];
         nums[i-1] = nums[minId];
         nums[minId] = tmp;
         
+        //step 3
+        //after step 2 done, nums[i...len-1] is still in descending order
+        //reverse it so that new number is minimum
         reverse(nums, i, nums.length-1);
         
         
