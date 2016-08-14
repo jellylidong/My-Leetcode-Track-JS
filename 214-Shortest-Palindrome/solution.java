1da2379c0302ba0a1f55321c8a2ea47eb35eb816
Dong <jellylidong@users.noreply.github.com>
2016-08-14 12:46:30 -0700

    [214. Shortest Palindrome][Accepted]committed by LeetCode Extension

--- 214-Shortest-Palindrome/solution.java
+++ 214-Shortest-Palindrome/solution.java
@@ -1,47 +1,51 @@
 public class Solution {
     public String shortestPalindrome(String s) {
-        if(s.length() == 0) return "";
+        // if(s.length() == 0) return "";
         
-        int[] kmpS = new int[s.length()];
+        // int[] kmpS = new int[s.length()];
+        // int j = 0;
+        // for(int i = 1; i < s.length(); i++){
+        //     if(s.charAt(i) == s.charAt(j)){
+        //         kmpS[i] = 1 + kmpS[i-1];
+        //         j++;
+        //     }
+        //     else{
+        //         while(j != 0 && s.charAt(i) != s.charAt(j))
+        //             j = kmpS[j-1];
+        //         if(s.charAt(i) == s.charAt(j)){
+        //             kmpS[i] = j+1;
+        //             j++;
+        //         }
+        //     }
+        // }
+        
+        // String r = new StringBuilder(s).reverse().toString();
+        // int[] kmpR = new int[r.length()];
+        // j = 0;
+        // for(int i = 0; i < r.length(); i++){
+        //     if(s.charAt(j) == r.charAt(i)){
+        //         kmpR[i] = j==0? 1:kmpR[i-1]+1;
+        //         j++;
+        //     }
+        //     else{
+        //         while(j != 0 && s.charAt(j) != r.charAt(i))
+        //             j = kmpS[j-1];
+        //         if(s.charAt(j) == r.charAt(i)){
+        //             kmpR[i] = j+1;
+        //             j++;
+        //         }
+        //     }
+        // }
+        // return r.substring(0, r.length()-kmpR[r.length()-1]) + s;
         int j = 0;
-        for(int i = 1; i < s.length(); i++){
-            if(s.charAt(i) == s.charAt(j)){
-                kmpS[i] = 1 + kmpS[i-1];
+        for(int i = s.length()-1; i >= 0; i--){
+            if(s.charAt(i) == s.charAt(j))
                 j++;
-            }
-            else{
-                while(j != 0 && s.charAt(i) != s.charAt(j))
-                    j = kmpS[j-1];
-                if(s.charAt(i) == s.charAt(j)){
-                    kmpS[i] = j+1;
-                    j++;
-                }
-            }
         }
+        if(j == s.length()) return s;
         
-        String r = new StringBuilder(s).reverse().toString();
-        int[] kmpR = new int[r.length()];
-        j = 0;
-        for(int i = 0; i < r.length(); i++){
-            if(s.charAt(j) == r.charAt(i)){
-                kmpR[i] = j==0? 1:kmpR[i-1]+1;
-                j++;
-            }
-            else{
-                while(j != 0 && s.charAt(j) != r.charAt(i))
-                    j = kmpS[j-1];
-                if(s.charAt(j) == r.charAt(i)){
-                    kmpR[i] = j+1;
-                    j++;
-                }
-            }
-        }
-        
-        
-        
-        // String rev = new StringBuilder(s).reverse().toString();
-        
-        return r.substring(0, r.length()-kmpR[r.length()-1]) + s;
+        String suffix = s.substring(j);
+        return new StringBuilder(suffix).reverse().append(shortestPalindrome(s.substring(0, j))).append(suffix).toString();
     }
     
     
