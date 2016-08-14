a0729567a846003d4f227c5a63211f8e5f6c57b9
Dong <jellylidong@users.noreply.github.com>
2016-08-14 15:13:19 -0700

    [231. Power of Two][Accepted]committed by LeetCode Extension

--- 231-Power-of-Two/solution.java
+++ 231-Power-of-Two/solution.java
@@ -1,10 +1,19 @@
 public class Solution {
     public boolean isPowerOfTwo(int n) {
         if(n < 1)  return false;
-        while(n%2 == 0){
-            n >>= 1;
-        }
         
-        return n == 1;
+        //method 1: devide by 2
+        // while(n%2 == 0){
+        //     n >>= 1;
+        // }
+        // return n == 1;
+        
+        //method 2: bit count
+        //for a power-of-2 number, only the left most bit is set, 
+        //ie it's form is X01000...0B, so num-1 =x00111...1B
+        //so num&(num-1) == 0
+        
+        return (n&(n-1)) == 0;
+        
     }
 }
