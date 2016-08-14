b6719d142a19e50c7f242d328ad2d63b8077ed5a
Dong <jellylidong@users.noreply.github.com>
2016-08-13 21:17:10 -0700

    [162. Find Peak Element][Accepted]committed by LeetCode Extension

--- 162-Find-Peak-Element/solution.java
+++ 162-Find-Peak-Element/solution.java
@@ -1,14 +1,28 @@
 public class Solution {
     public int findPeakElement(int[] nums) {
-        if(nums.length == 1)    return 0;
-        if(nums[0] > nums[1])   return 0;
-        if(nums[nums.length-1] > nums[nums.length-2])   return nums.length-1;
+        // if(nums.length == 1)    return 0;
+        // if(nums[0] > nums[1])   return 0;
+        // if(nums[nums.length-1] > nums[nums.length-2])   return nums.length-1;
         
-        for(int i = 1; i < nums.length-1; i++){
-            if(nums[i-1] < nums[i] && nums[i] > nums[i+1])
-                return i;
+        // for(int i = 1; i < nums.length-1; i++){
+        //     if(nums[i-1] < nums[i] && nums[i] > nums[i+1])
+        //         return i;
+        // }
+        
+        // return -1;
+        
+        //each local max is peak, 
+        //so we just go to the part that has a bigger value to find a peak
+        int lo = 0;
+        int hi = nums.length-1;
+        while(lo < hi){
+            int m1 = lo + (hi-lo)/2;
+            int m2 = m1+1;
+            
+            if(nums[m1] > nums[m2]) hi = m1;
+            else    lo = m2;
         }
         
-        return -1;
+        return lo;
     }
 }
