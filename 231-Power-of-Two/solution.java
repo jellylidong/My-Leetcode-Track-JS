9e40407a3017913f0802575ecf0a526afc21b0e7
Dong <jellylidong@users.noreply.github.com>
2016-08-14 15:08:45 -0700

    [231. Power of Two][Accepted]committed by LeetCode Extension

--- 231-Power-of-Two/solution.java
+++ 231-Power-of-Two/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public boolean isPowerOfTwo(int n) {
         if(n < 1)  return false;
         while(n%2 == 0){
-            n /= 2;
+            n >>= 1;
         }
         
         return n == 1;
