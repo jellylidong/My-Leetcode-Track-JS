ab1d36f5d3424d8428efd7051e5c36500ef2b95b
Dong <jellylidong@users.noreply.github.com>
2016-08-13 23:16:32 -0700

    [212. Word Search II][Accepted]committed by LeetCode Extension

--- 212-Word-Search-II/solution.java
+++ 212-Word-Search-II/solution.java
@@ -68,24 +68,34 @@ public class Solution {
     
     public void search(char[][] board, boolean[][] visited, int i, int j, Trie cur, List<String> ansSet){
         if(cur == null)    return;
-        if(cur.isStr){
+        // if(cur.isStr){
             
-            ansSet.add(cur.str);
-            cur.isStr = false;
-            //change cur.isStr to false after we add it to answer so that we don't have to add it again
-            //once we can avoid add dup, we can add string to list directly instead of HashSet
+        //     ansSet.add(cur.str);
+        //     cur.isStr = false;
+        //     //change cur.isStr to false after we add it to answer so that we don't have to add it again
+        //     //once we can avoid add dup, we can add string to list directly instead of HashSet
+        // }
+        
+        // if(i < 0 || i == board.length || j < 0 || j == board[0].length) return;
+        // if(visited[i][j])   return;
+        
+        
+        cur = cur.curChar[board[i][j]-'a'];
+        if(cur != null){
+            if(cur.isStr){
+                ansSet.add(cur.str);
+                cur.isStr = false;
+            }
+            visited[i][j] = true;
+            if(i-1 >= 0 && !visited[i-1][j])
+                search(board, visited, i-1, j, cur, ansSet);
+            if(i+1 < board.length && !visited[i+1][j])
+                search(board, visited, i+1, j, cur, ansSet);
+            if(j-1 >= 0 && !visited[i][j-1])
+                search(board, visited, i, j-1, cur, ansSet);
+            if(j+1 < board[0].length && !visited[i][j+1])
+                search(board, visited, i, j+1, cur, ansSet);
+            visited[i][j] = false;
         }
-        
-        if(i < 0 || i == board.length || j < 0 || j == board[0].length) return;
-        if(visited[i][j])   return;
-        
-        
-        
-        visited[i][j] = true;
-        search(board, visited, i-1, j, cur.curChar[board[i][j]-'a'], ansSet);
-        search(board, visited, i+1, j, cur.curChar[board[i][j]-'a'], ansSet);
-        search(board, visited, i, j-1, cur.curChar[board[i][j]-'a'], ansSet);
-        search(board, visited, i, j+1, cur.curChar[board[i][j]-'a'], ansSet);
-        visited[i][j] = false;
     }
 }
