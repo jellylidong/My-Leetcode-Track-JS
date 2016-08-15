b32494bcb9cdb1b5980513f963bb3b3ed2787b71
Dong <jellylidong@users.noreply.github.com>
2016-08-15 00:28:46 -0700

    [284. Peeking Iterator][Accepted]committed by LeetCode Extension

--- 284-Peeking-Iterator/solution.java
+++ 284-Peeking-Iterator/solution.java
@@ -2,31 +2,34 @@
 // https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
 class PeekingIterator implements Iterator<Integer> {
 
-    Queue<Integer> buff;
     Iterator<Integer> iterator;
+    Integer buff;
 	public PeekingIterator(Iterator<Integer> iterator) {
 	    // initialize any member here.
-	    this.buff = new LinkedList<>();
 	    this.iterator = iterator;
+	    this.buff = null;
 	}
 
     // Returns the next element in the iteration without advancing the iterator.
 	public Integer peek() {
-        if(buff.isEmpty() && iterator.hasNext())
-            buff.offer(iterator.next());
-        return buff.peek();
+        if(buff == null)    buff = iterator.next();
+        return buff;
 	}
 
 	// hasNext() and next() should behave the same as in the Iterator interface.
 	// Override them if needed.
 	@Override
 	public Integer next() {
-	    if(!buff.isEmpty()) return buff.poll();
-	    else return iterator.next();
+	    if(buff != null){
+	        int tmp = buff;
+	        buff = null;
+	        return tmp;
+	    }
+	    return iterator.next();
 	}
 
 	@Override
 	public boolean hasNext() {
-	    return !buff.isEmpty() || iterator.hasNext();
+	    return buff != null || iterator.hasNext();
 	}
 }
