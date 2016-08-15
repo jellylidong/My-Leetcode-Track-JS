a9827e25ed365907c70db2b1e94c7a209be766dd
Dong <jellylidong@users.noreply.github.com>
2016-08-14 18:12:38 -0700

    [326. Power of Three][Accepted]committed by LeetCode Extension

--- 326-Power-of-Three/solution.java
+++ 326-Power-of-Three/solution.java
@@ -1,6 +1,7 @@
 public class Solution {
     public boolean isPowerOfThree(int n) {
         if(n <= 0)  return false;
+        int t = (int)Math.pow(2, Math.log(Integer.MAX_VALUE)/Math.log(3));
         return 1162261467%n == 0;
         //1162261467 is the largest power3 integer
     }
