037968155036768814aa113c054ff118602fb225
Dong <jellylidong@users.noreply.github.com>
2016-08-17 10:45:14 -0700

    [274. H-Index][Accepted]committed by LeetCode Extension

--- 274-H-Index/solution.java
+++ 274-H-Index/solution.java
@@ -3,8 +3,11 @@ public class Solution {
         Arrays.sort(citations);
         int h = 0;
         for(int i = 0; i < citations.length; i++){
-            if(citations[i] >= citations.length-i)
-                h = Math.max(h, citations.length-i);
+            if(citations[i] >= citations.length-i){
+                // h = Math.max(h, citations.length-i);
+                h = citations.length-i;
+                break;
+            }
         }
         
         return h;
