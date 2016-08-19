c0a5fedcffc3502ed740b3325dc80223c3d537cc
Dong <jellylidong@users.noreply.github.com>
2016-08-18 18:57:29 -0700

    [320. Generalized Abbreviation][Accepted]committed by LeetCode Extension

---
+++ 320-Generalized-Abbreviation/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public List<String> generateAbbreviations(String word) {
+        List<String> ans = new ArrayList<>();
+        helper(ans, word, "", 0, 0);
+        return ans;
+    }
+    
+    public void helper(List<String> ans, String word, String cur, int p, int count){
+        if(p == word.length()){
+            if(count > 0)   cur += count;
+            ans.add(cur);
+        }
+        else{
+            //not abbr
+            helper(ans, word, cur + (count > 0? count:"") + word.charAt(p), p+1, 0);
+            
+            //abbr
+            helper(ans, word,cur, p+1, count+1);
+        }
+    }
+}
