28efb586111987d0121ff7b94bb26aa471f29759
Dong <jellylidong@users.noreply.github.com>
2016-08-19 15:37:16 -0700

    [277. Find the Celebrity][Accepted]committed by LeetCode Extension

--- 277-Find-the-Celebrity/solution.java
+++ 277-Find-the-Celebrity/solution.java
@@ -3,20 +3,23 @@
 
 public class Solution extends Relation {
     public int findCelebrity(int n) {
-        int ans = -1;
+        int ans = 0;
+        
+        for(int i = 1; i <= n-1; i++){
+            if(knows(ans, i))
+                ans = i; //once we find the celebrity, since all people knows it, so it won't change
+        }
+        
+        //but for case 0 does not know 1; 1 does not know 0
+        //there is no celebrity actually, so we need to check if it's a real celebrity
         for(int i = 0; i < n; i++){
-            int count = 0;
-            for(int j = 0; j < n; j++){
-                if(i != j){
-                    if(knows(j, i) && !knows(i, j))
-                        count++;
-                    if(count == n-1){
-                        ans = i;
-                        break;
-                    }
-                }
+            if(i != ans){
+                if(knows(ans, i) || !knows(i, ans))
+                    return -1;
             }
         }
+        
         return ans;
+        
     }
 }
