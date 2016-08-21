0248c8f12b0dc8318f6a445ae78aef5b900e7725
Dong <jellylidong@users.noreply.github.com>
2016-08-21 10:37:26 -0700

    [368. Largest Divisible Subset][Accepted]committed by LeetCode Extension

--- 368-Largest-Divisible-Subset/solution.java
+++ 368-Largest-Divisible-Subset/solution.java
@@ -1,32 +1,36 @@
 public class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        Arrays.sort(nums);
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-        
-        int maxSize = 0;
+        int len = nums.length;
         List<Integer> ans = new ArrayList<>();
+        if(len < 2){ 
+            for(int n: nums)    ans.add(n);
+            return ans;
+        }
         
-        for(int i = 0; i < nums.length; i++){
-            
-            List<Integer> curList = new ArrayList<>();
-            curList.add(nums[i]);
-            
-            List<Integer> preList = new ArrayList<>();
-            int preMaxSize = 0;
-            for(int j = i-1; j >= 0; j--){
-                if(nums[i]%nums[j] == 0 && preMaxSize < map.get(nums[j]).size()){
-                    preList = map.get(nums[j]);
-                    preMaxSize = preList.size();
+        int[] count = new int[len]; //count[i] means i has count[i] dividend //bei chushu
+        int[] parent = new int[len]; //parent[i] is the id of its dividend with max subset //bei chu shu
+        
+        Arrays.sort(nums);
+        int maxSize = 0;
+        int maxId = 0;
+        
+        for(int i = len-1; i >= 0; i--){
+            for(int j = i; j < len; j++){ //j >= i
+                if(nums[j]%nums[i] == 0 && count[i] < 1 + count[j]){
+                    count[i] = 1+count[j];
+                    parent[i] = j;
+                    if(count[i] > maxSize){
+                        maxSize = count[i];
+                        maxId = i;
+                    }
                 }
-            }
-            curList.addAll(preList);
-            map.put(nums[i], curList);
-            if(curList.size() > maxSize){
-                maxSize = curList.size();
-                ans = curList;
             }
         }
         
+        for(int i = 0; i < maxSize; i++){
+            ans.add(nums[maxId]);
+            maxId = parent[maxId];
+        }
         return ans;
     }
 }
