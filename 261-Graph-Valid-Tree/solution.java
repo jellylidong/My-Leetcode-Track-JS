21a87c65bf621deed9b57d4654dc86eb1a36efa5
Dong <jellylidong@users.noreply.github.com>
2016-08-21 21:46:52 -0700

    [261. Graph Valid Tree][Accepted]committed by LeetCode Extension

--- 261-Graph-Valid-Tree/solution.java
+++ 261-Graph-Valid-Tree/solution.java
@@ -1,7 +1,9 @@
 public class Solution {
     public boolean validTree(int n, int[][] edges) {
         int[] root = new int[n]; //root[i] is i's parent
-        Arrays.fill(root, -1);
+        // Arrays.fill(root, -1);
+        for(int i = 0; i < n; i++)
+            root[i] = i;
         
         for(int[] pair:edges){
             int n1 = pair[0];
@@ -16,12 +18,19 @@ public class Solution {
                 else    root[r2] = r1;
             }
         }
-        
+        //can not return true directly
+        //for a valid graph tree, there are two conditions
+        //1.no circle
+        //2.all nodes are connected, not isolated
         return n-1 == edges.length;
     }
     
     public int find(int[] root, int target){
-        if(root[target] == -1)  return target;
-        return find(root, root[target]);
+        while(target != root[target]){
+            int father = root[root[target]];
+            root[target] = father;
+            target = root[target];
+        }
+        return target;
     }
 }
