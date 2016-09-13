76f8acbd46fbc8a53ca0809da7b724d23a8679e6
Dong <jellylidong@users.noreply.github.com>
2016-09-12 17:02:54 -0700

    [334. Increasing Triplet Subsequence][Accepted]committed by LeetCode Extension

--- 334-Increasing-Triplet-Subsequence/solution.java
+++ 334-Increasing-Triplet-Subsequence/solution.java
@@ -1,34 +1,13 @@
 public class Solution {
     public boolean increasingTriplet(int[] nums) {
-        if(nums.length <= 2)    return false;
-        int minId = findMinId(nums, 0, 0);
-        int maxId = findMaxId(nums, 2, nums.length-1);
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
         
-        for(int i = 1; i <= nums.length-2; i++){
-            if(i == maxId) maxId = findMaxId(nums, maxId+1, nums.length-1);
-            if(nums[minId] < nums[i] && nums[i] < nums[maxId])  return true;
-            if(nums[i] < nums[minId])   minId = i;
-            
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] < min1)  min1 = nums[i];
+            else if(nums[i] <min2 && nums[i] > min1)    min2 = nums[i];
+            else if(nums[i] > Math.max(min1, min2))  return true;
         }
-        
         return false;
-    }
-    
-    public int findMinId(int[] nums, int lo, int hi){
-        int minId = lo;
-        for(int i = lo+1; i <= hi; i++){
-            if(nums[i] < nums[minId])
-                minId = i;
-        }
-        return minId;
-    }
-    
-    public int findMaxId(int[] nums, int lo, int hi){
-        int maxId = lo;
-        for(int i = lo+1; i <= hi; i++){
-            if(nums[i] >= nums[maxId])
-                maxId = i;
-        }
-        return maxId;
     }
 }
