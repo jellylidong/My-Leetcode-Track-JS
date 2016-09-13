9b7d6bd0b64d39d6eeb5048a12f4ec037881f8de
Dong <jellylidong@users.noreply.github.com>
2016-09-13 11:01:14 -0700

    [330. Patching Array][Accepted]committed by LeetCode Extension

--- 330-Patching-Array/solution.java
+++ 330-Patching-Array/solution.java
@@ -1,44 +1,29 @@
 public class Solution {
     public int minPatches(int[] nums, int n) {
-        int i = 0;
-        long miss = 1; 
-        //exclusive bound, miss is the upper bound that nums[0...i] can not reach, i is exclusive
-        //note: the upper bound can bigger than int_max, so we use long
-        int count = 0;
+        int i = 0; //beginning index
+        long miss = 1; //current upper bound
+        int count = 0; //count of number patched
         
-        // while(miss <= n){
-        //     if(i < nums.length && nums[i] <= miss){
-        //         miss += nums[i];
-        //         i++; //
-        //     }
-        //     else{
-        //         miss += miss;
-        //         count++;
-        //     }
-        // }
-        
-        // return count;
-        
+        // if(nums.length == 0) //for corner case no number is is given
+        //         nums = new int[] {n};
+
         while(miss <= n){
             if(i == nums.length || (i < nums.length && nums[i] > miss)){
-                //target is nums[i], but we can only reach miss-1
-                //so we shuold patch miss, 
-                //then the the upper bound we can not reach becuomes miss+miss
-                //so:
+                //we want to reach nums[i], but we currently we only reach miss-1
+                //so we should patch miss to extend to the largest upper bound
+                //after patching, we can reach miss-1+miss
+                //so the upper bound should be miss += miss
                 miss += miss;
-                count++;
+                count++; 
             }
             else if(i < nums.length && nums[i] <= miss){
-                //nums[i] <= miss
-                //we reached nums[i]
-                //but we can already reach miss-1,
-                //so the real upper bould shuold be miss+nums[i];
+                //we want to reach nums[i], but we already reach miss-1
+                //so we can actually reach nums[i]+miss-1
+                //so the upper bound should be nums[i]+miss
                 miss += nums[i];
                 i++;
             }
-            
         }
-        
         return count;
-    }
+}
 }
