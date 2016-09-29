9cdc328791b25864e96c6d37f035849e9f900372
Dong <jellylidong@users.noreply.github.com>
2016-09-29 15:25:11 -0700

    [42. Trapping Rain Water][Accepted]committed by LeetCode Extension

--- 42-Trapping-Rain-Water/solution.java
+++ 42-Trapping-Rain-Water/solution.java
@@ -1,48 +1,24 @@
 public class Solution {
-    //method 1: array
-    // public int trap(int[] height) {
-    //     int ans = 0;
-        
-    //     int lo = 0;
-    //     int hi = height.length-1;
-        
-    //     while(lo < hi){
-    //         int left = height[lo];
-    //         int right = height[hi];
-            
-    //         if(left <= right){
-    //             while(lo <= hi && left >= height[lo]){
-    //                 ans += (left - height[lo]);
-    //                 lo++;
-    //             }
-    //         }
-    //         else {
-    //             while(lo <= hi && right >= height[hi]){
-    //                 ans += (right - height[hi]);
-    //                 hi--;
-    //             }
-    //         }
-    //     }
-        
-    //     return ans;
-    // }
-    
-    
-    //method 2: stack
     public int trap(int[] height) {
-        Stack<Integer> stack = new Stack<>(); //stores index of height
-        int ans = 0;
-        int i = 0;
-        while(i < height.length){
-            if(stack.isEmpty() || height[stack.peek()] >= height[i]){
-                stack.push(i++);
-            }
-            else{
-                int pre = stack.pop();
-                int delta = stack.isEmpty()? 0:(Math.min(height[stack.peek()], height[i]) - height[pre])*(i-stack.peek()-1);
-                ans += delta;        
-            }
+        Stack<Integer> stack = new Stack<>();
+        int res = 0;
+        for(int i = 0; i < height.length; i++){
+        	if(stack.isEmpty() || height[i] <= height[stack.peek()]){
+        		stack.push(i);
+        	}
+        	else{
+        		while(!stack.isEmpty() && height[i] >= height[stack.peek()]){
+        			int pre = stack.pop();
+        			if(!stack.isEmpty()){
+        				int dh = Math.min(height[i],  height[stack.peek()]) - height[pre];
+        				int dx = i - stack.peek()-1;
+        				res += dh*dx;
+        			}
+        		}
+        		stack.push(i);
+        	}
         }
-        return ans;
+        
+        return res;
     }
 }
