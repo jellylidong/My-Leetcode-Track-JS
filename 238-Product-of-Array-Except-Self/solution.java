239651b0b614e3f654eeaad32d3054b54764443b
Dong <jellylidong@users.noreply.github.com>
2016-09-30 14:44:00 -0700

    [238. Product of Array Except Self][Accepted]committed by LeetCode Extension

--- 238-Product-of-Array-Except-Self/solution.java
+++ 238-Product-of-Array-Except-Self/solution.java
@@ -1,6 +1,7 @@
 public class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] ans = new int[nums.length];
+        //ans[i] is the product of nums[0...(i-1)], both inclusive
         ans[0] = 1;
         for(int i = 1; i < nums.length; i++){
             ans[i] = ans[i-1] * nums[i-1];
