6769a977391cefc86c9b7b4a68050bd978556347
Dong <jellylidong@users.noreply.github.com>
2016-09-30 16:28:48 -0700

    [215. Kth Largest Element in an Array][Accepted]committed by LeetCode Extension

--- 215-Kth-Largest-Element-in-an-Array/solution.java
+++ 215-Kth-Largest-Element-in-an-Array/solution.java
@@ -1,29 +1,24 @@
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int index = quickSelect(nums, 0, nums.length-1, nums.length-k+1);
-        return nums[index];
+        return quickSelect(nums, 0, nums.length-1, nums.length-k); //the kth, k starts from 0
     }
     
     public int quickSelect(int[] nums, int lo, int hi, int k){
-        //looking for the kth number between lo and hi, both lo and hi are inclusive and k starts from 1
-        int i = lo;
-        int j = hi;
         int pivot = nums[hi];
-        while(i < j){
-            if(nums[i] > pivot){
-                j--;
-                swap(nums, i, j);
-            }
-            else{    i++;}
-        }
-        // System.out.println(i + " " + j);
-        swap(nums, i, hi);
+        int s = lo;
+        int e = hi;
         
-        int count = i-lo+1; //count of numbers <= pivot
-        // System.out.println("count: " + count);
-        if(count == k)  return i; 
-        else if(count < k)  return quickSelect(nums, i+1, hi, k-count);
-        else    return quickSelect(nums, lo, i-1, k);
+        while(s < e){
+            if(nums[s] > pivot){
+                e--;
+                swap(nums, s, e);
+            }
+            else s++;
+        }
+        swap(nums, s, hi);
+        if(lo+k == s)   return nums[s];
+        else if(lo+k < s)   return quickSelect(nums, lo, s-1, k);
+        else return quickSelect(nums, s+1, hi, k-(s-lo+1));
     }
     
     public void swap(int[] nums, int i, int j){
