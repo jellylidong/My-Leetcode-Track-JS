df99a1ff23e40835a0b9e339210505d95c1574c8
Dong <jellylidong@users.noreply.github.com>
2016-09-30 11:28:24 -0700

    [3. Longest Substring Without Repeating Characters][Accepted]committed by LeetCode Extension

--- 3-Longest-Substring-Without-Repeating-Characters/solution.java
+++ 3-Longest-Substring-Without-Repeating-Characters/solution.java
@@ -7,6 +7,8 @@ public class Solution {
         for(int hi = 0; hi < s.length(); hi++){
             char c = s.charAt(hi);
             if(map.containsKey(c)){
+                //for case like "abba"
+                //when we get 2nd b, the lo is already updated that makes no duplicate repeating a, so we should use bigger lo
                 lo = Math.max(lo, map.get(c)+1);
             }
             map.put(c, hi);
